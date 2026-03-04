78fcd55c928ec588cf9380040a06f4bcbb4cfd64
syntax <156238853+syntaxiax@users.noreply.github.com>
2026-03-04 17:57:34 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,7 +12,7 @@ intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
 
-bot = commands.Bot(command_prefix=commands.when_mentioned, intents=intents)
+bot = commands.Bot(command_prefix="?", intents=intents)
 
 SKIP = {"giveaway_utils"}
 
